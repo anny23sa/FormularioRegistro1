18f0317917a04b26914c6cc3d6704e360f94ddf4
Anny Stecca <anny23sa@gmail.com>
2023-09-24 17:00:02 -0700

    Creacion De componentes

---
+++ src/components/Registro/Registro.jsx
@@ -0,0 +1,20 @@
+import App from './App'
+import React from 'react'
+import SocialButton from './SocialButton/SocialButton.jsx'
+import Formulario from './Formulario/Formulario.jsx';
+import Alert from './Alert/alert.jsx';
+
+const Registro = () => {
+  return (
+    <div>
+      <h1>Registro</h1>
+        <SocialButton />
+        <Formulario />
+        <Alert />
+    </div>
+  );
+}
+
+export default Registro
+
+
---
+++ src/components/SocialButton/SocialButton.jsx
@@ -0,0 +1,12 @@
+
+import Alert from 'Alert'
+import Formulario from 'Formulario'
+import Registro from 'Registro'
+
+const SocialButton = () => {
+  return (
+    <div>SocialButton</div>
+  )
+}
+
+export default SocialButton
